c61c2be3c9974d56ae00e4af04698ff9bf9ccf67
marco rojouan <marcoprojouan@outlook.fr>
2024-10-08 13:57:08 +0200

    Update : all

--- bouclejs/script.js
+++ bouclejs/script.js
@@ -1,1 +1,21 @@
+const numberOne = 1;
+const numberTwo = 3;
+const numberThree = 2;
 
+if (numberOne == numberTwo &&
+    numberTwo == numberThree
+) {
+  console.log("Les trois variables sont identiques");
+} else if (
+  numberOne == numberTwo ||
+  numberOne == numberThree ||
+  numberTwo == numberThree
+) {
+  console.log("Deux variables sont identiques");
+} else if (
+  numberOne != numberTwo &&
+  numberOne != numberThree &&
+  numberTwo != numberThree
+) {
+  console.log("Toutes les variables sont differentes");
+}
